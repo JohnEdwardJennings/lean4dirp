159beb16723dbc200fd8dad75df5a835094b235a
John Jennings <john@jennings62.com>
2025-09-26 18:53:11 -0500

    add week 2 exercises

---
+++ Lean4dirp/week2.lean
@@ -0,0 +1,46 @@
+/- Lean4 Exercises for Week 2. -/
+
+import Mathlib
+
+/- Define a function that adds 3 to an natural number. -/
+def add_three : ℕ → ℕ := sorry
+
+#eval add_three 0  /- 3 -/
+#eval add_three 1  /- 4 -/
+#eval add_three 10  /- 13 -/
+
+/- Define a function that subtracts 1 from an integer. -/
+def sub_one : ℤ → ℤ := sorry
+
+#eval sub_one 0  /- -1 -/
+#eval sub_one (-1)  /- -2 -/
+#eval sub_one 5  /- 4 -/
+
+/- Define a function that subtracts an integer from 1. -/
+def one_sub : ℤ → ℤ := sorry
+
+#eval one_sub 0  /- 1 -/
+#eval one_sub (-1)  /- 2 -/
+#eval one_sub 5  /- -4 -/
+
+/- Bonus: can you define the first two functions above without using `fun`? -/
+
+/- Define a function `applyAndAdd` that takes a function and two natural numbers,
+applies the function to the first natural number, and adds the result to the second
+natural number.
+No stub is provided; you must state the type of `f` yourself. -/
+
+#eval applyAndAdd (fun n => 3 * n ^ 2) 2 5 /- 17 -/
+
+
+/- Prove the following two theorems. Above each theorem, add a documentation comment
+like the one provided for `triv` below explaining what the theorem proves logically. -/
+
+/-- This theorem is trivial. It proves "True", which logically should be trivial,
+and indeed the proof for "True" already exists in Lean 4.
+That proof, a term of type `True`, is named `True.intro`. -/
+theorem triv : True := True.intro
+
+theorem ident {a : Prop} (t : a) : a := sorry
+
+theorem composition {a b c : Prop} (f : a → b) (g : b → c) : a → c := sorry
