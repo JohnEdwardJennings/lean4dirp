a6d7df9449d8d3569f88ff8b8571a89c023f4f10
Maxim Rebguns <mrmaxguns@gmail.com>
2025-10-06 09:42:30 -0500

    completed week 2 exercises

--- Lean4dirp/week2.lean
+++ Lean4dirp/week2.lean
@@ -3,21 +3,21 @@
 import Mathlib
 
 /- Define a function that adds 3 to an natural number. -/
-def add_three : ℕ → ℕ := sorry
+def add_three : ℕ → ℕ := λ x => x + 3
 
 #eval add_three 0  /- 3 -/
 #eval add_three 1  /- 4 -/
 #eval add_three 10  /- 13 -/
 
 /- Define a function that subtracts 1 from an integer. -/
-def sub_one : ℤ → ℤ := sorry
+def sub_one : ℤ → ℤ := λ x => x - 1
 
 #eval sub_one 0  /- -1 -/
 #eval sub_one (-1)  /- -2 -/
 #eval sub_one 5  /- 4 -/
 
 /- Define a function that subtracts an integer from 1. -/
-def one_sub : ℤ → ℤ := sorry
+def one_sub : ℤ → ℤ := Sub.sub 1
 
 #eval one_sub 0  /- 1 -/
 #eval one_sub (-1)  /- 2 -/
@@ -30,6 +30,8 @@ applies the function to the first natural number, and adds the result to the sec
 natural number.
 No stub is provided; you must state the type of `applyAndAdd` yourself. -/
 
+def applyAndAdd (f : ℕ → ℕ) (a b : ℕ) : ℕ := (f a) + b
+
 #eval applyAndAdd (fun n => 3 * n ^ 2) 2 5 /- 17 -/
 
 
@@ -41,6 +43,8 @@ and indeed the proof for "True" already exists in Lean 4.
 That proof, a term of type `True`, is named `True.intro`. -/
 theorem triv : True := True.intro
 
-theorem ident {a : Prop} (t : a) : a := sorry
+/-- If we know a is true, then a is true. -/
+theorem ident {a : Prop} (t : a) : a := t
 
-theorem composition {a b c : Prop} (f : a → b) (g : b → c) : a → c := sorry
+/-- If a implies b and b implies c, then a implies c. -/
+theorem composition {a b c : Prop} (f : a → b) (g : b → c) : a → c := g ∘ f
