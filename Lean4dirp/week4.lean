02f79c92724edbf4387c4d35ec7344f1b873a0c1
Maxim Rebguns <mrmaxguns@gmail.com>
2025-10-17 20:00:42 -0500

    week 4

--- Lean4dirp/week4.lean
+++ Lean4dirp/week4.lean
@@ -11,7 +11,11 @@ namespace L -- enables the notation `nil`, `cons` instead of `L.nil`, `L.cons`.
 
 Given a function `f : α → β` and a list `l` of terms of type `α`,
 produce a list of the values of `f` applied to each term of `l` in order. -/
-def map {α β : Type} (f : α → β) : L α → L β := sorry
+def map {α β : Type} (f : α → β) : L α → L β := fun l =>
+  match l with
+  | nil => nil
+  | cons a as => cons (f a) (map f as)
+
 
 #eval map (fun (n : Nat) => n + 1) nil -- nil
 #eval map (fun (n : Nat) => n + 1) (cons 2 nil) -- cons 3 nil
@@ -25,7 +29,12 @@ def map {α β : Type} (f : α → β) : L α → L β := sorry
 Given a predicate `p : α → Bool` and a list `l` of terms of type `α`,
 return a list of the terms `a` of the list satisfying `p a`, in order.
 -/
-def filter {α : Type} (p : α → Bool) : L α → L α := sorry
+def filter {α : Type} (p : α → Bool) : L α → L α := fun l =>
+  match l with
+  | nil => nil
+  | cons a as => if (p a)
+      then cons a (filter p as)
+      else filter p as
 
 #eval filter (fun n => n < 5) nil -- nil
 #eval filter (fun n => n < 5) (cons 4 nil) -- cons 4 nil
@@ -41,7 +50,10 @@ as well as a starting value `b` of type `β`,
 applying `g` repeatedly to the value of type `β`, passing in arguments
 of type `α` from the tail to the head.
 -/
-def foldl {α β : Type} (g : α → β → β) (b : β) : L α → β := sorry
+def foldl {α β : Type} (g : α → β → β) (b : β) : L α → β := fun l =>
+  match l with
+  | nil => b
+  | cons a as => g a (foldl g b as)
 
 #eval foldl Add.add 0 (cons 1 (cons 2 (cons 3 (cons 4 nil)))) -- 10
 #eval foldl Mul.mul 1 (cons 1 (cons 2 (cons 3 (cons 4 nil)))) -- 24
@@ -50,7 +62,10 @@ def foldl {α β : Type} (g : α → β → β) (b : β) : L α → β := sorry
 
 /-- The same as `foldl`, but the elements should be folded in from the
 head to the tail. -/
-def foldr {α β : Type} (g : α → β → β) (b : β) : L α → β := sorry
+def foldr {α β : Type} (g : α → β → β) (b : β) : L α → β := fun l =>
+  match l with
+  | nil => b
+  | cons a as => foldr g (g a b) as
 
 #eval foldr Add.add 0 (cons 1 (cons 2 (cons 3 (cons 4 nil)))) -- 10
 #eval foldr Mul.mul 1 (cons 1 (cons 2 (cons 3 (cons 4 nil)))) -- 24
@@ -63,7 +78,7 @@ Reverse a list in linear time.
 Hint: `foldl` and `foldr` operate on the list in reverse order of
 each other, and `foldl cons nil` is equal to `id`.
 -/
-def reverse {α : Type} : L α → L α := sorry
+def reverse {α : Type} : L α → L α := foldr cons nil
 
 #eval reverse (cons 1 (cons 2 (cons 3 (cons 4 nil)))) -- cons 4 (cons 3 (cons 2 (cons 1 nil)))
 #eval reverse (cons "hello" (cons "lean" (cons "world" nil))) -- cons "world" (cons "lean" (cons "hello" nil))
@@ -75,7 +90,14 @@ should contain all the elements of the original list of lists,
 in the order as they appear if the nested lists were concatenated
 end to end.
 -/
-def flatten {α : Type} : L (L α) → L α := sorry
+def flatten {α : Type} : L (L α) → L α := fun l =>
+  match l with
+  | nil => nil
+  | cons a as =>
+    match a with
+    | nil => flatten as
+    | cons b bs => cons b (flatten (cons bs as))
+
 
 #eval flatten (α := Nat) nil -- nil
 #eval flatten (cons (cons 1 nil) nil) -- cons 1 nil
@@ -90,7 +112,13 @@ from those lists, with each element from the first list matched with
 the element in the same position in the second list. If one list is
 longer than the other, ignore any leftover elements in the longer list.
 -/
-def zip {α β : Type} : L α → L β → L (α × β) := sorry
+def zip {α β : Type} : L α → L β → L (α × β) := fun as bs =>
+  match as with
+  | nil => nil
+  | cons a as =>
+    match bs with
+    | nil => nil
+    | cons b bs => cons (a, b) (zip as bs)
 
 #eval zip (cons 1 (cons 2 nil)) (cons 3 (cons 4 nil)) -- cons (1, 3) (cons (2, 4) nil)
 #eval zip (cons 1 (cons 2 (cons 3 nil))) (cons 7 (cons 8 (cons 9 nil))) -- cons (1, 7) (cons (2, 8) (cons (3, 9) nil))
